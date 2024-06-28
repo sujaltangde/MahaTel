c6c376d98d264ed086b8315b4f9581fdf3929288
Sujal Tangde <sujaltagade01@gmail.com>
2024-06-28 21:56:55 +0530

    code added

--- client/src/pages/MainPage.tsx
+++ client/src/pages/MainPage.tsx
@@ -6,12 +6,7 @@ interface TokenDisplayStates {
   supply?: number;
 }
 
-interface NFTStates {
-  nftImage?: string | ArrayBuffer | null;
-  nftImgName?: string;
-  nftName?: string;
-  supply?: number;
-}
+
 
 interface MultisenderStates {
   contractAddress?: string;
@@ -23,7 +18,7 @@ const MainPage: React.FC = () => {
   const [pageState, setPageState] = useState<string>("1");
 
   const [tokenDisplayStates, setTokenDisplayStates] = useState<TokenDisplayStates>({});
-  const [nftStates, setNFTStates] = useState<NFTStates>({});
+  
   const [multisenderStates, setMultisenderStates] = useState<MultisenderStates>({});
 
   const handleTokenDisplay = (e: React.FormEvent<HTMLFormElement>) => {
@@ -32,32 +27,13 @@ const MainPage: React.FC = () => {
     setPageState("2");
   };
 
-  const handleDeployNFT = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log("nftStates", nftStates);
-    setPageState("3");
-  };
-
+  
   const handleMultisender = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     console.log("multisenderStates", multisenderStates);
   };
 
-  const nftChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    if (e.target.files && e.target.files[0]) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        if (reader.readyState === 2) {
-          setNFTStates((prevState) => ({
-            ...prevState,
-            nftImage: reader.result,
-            nftImgName: e.target.files![0].name,
-          }));
-        }
-      };
-      reader.readAsDataURL(e.target.files[0]);
-    }
-  };
+  
 
   return (
     <div className="bg-gray-950 min-h-screen text-white">
@@ -69,18 +45,11 @@ const MainPage: React.FC = () => {
         >
           Token Display
         </div>
-        <div className="border-white border w-44 hidden md:block"></div>
+        <div className="border-white border md:w-44 w-16  block"></div>
+        
         <div
           className={`${
             pageState === "2" && "bg-blue-900"
-          } md:text-xl text-xs border border-white px-8 py-3 rounded-3xl cursor-pointer`}
-        >
-          NFT's
-        </div>
-        <div className="border-white border w-44 hidden md:block"></div>
-        <div
-          className={`${
-            pageState === "3" && "bg-blue-900"
           } md:text-xl text-xs border border-white px-6 py-3 rounded-3xl cursor-pointer`}
         >
           Multisender
@@ -141,69 +110,10 @@ const MainPage: React.FC = () => {
         </div>
       )}
 
-      {/* NFT's */}
-      {pageState === "2" && (
-        <div className="flex justify-center items-center mt-8 w-full px-4">
-          <form
-            onSubmit={handleDeployNFT}
-            className="w-full max-w-md p-8 bg-gray-800 rounded-lg shadow-lg flex flex-col gap-6 items-center"
-          >
-            <label htmlFor="nftImg" className="cursor-pointer shadow appearance-none border rounded w-full py-2 px-3 text-white leading-tight focus:outline-none focus:shadow-outline">
-              {nftStates && nftStates.nftImgName ? nftStates.nftImgName.length > 24 ? nftStates.nftImgName.slice(0,24) + "..." : nftStates.nftImgName : " Select NFT Image"}
-            </label>
-            <input
-              type="file"
-              id="nftImg"
-              className="hidden"
-              name="nft"
-              onChange={nftChange}
-              required
-            />
-            {nftStates && nftStates.nftImage && (
-              <div className="mt-2 w-full flex justify-center">
-                <img
-                  src={nftStates.nftImage as string}
-                  alt="Selected NFT"
-                  className="w-32 h-32 object-cover rounded-md"
-                />
-              </div>
-            )}
-            <input
-              type="text"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="NFT Name"
-              onChange={(e) => {
-                setNFTStates({
-                  ...nftStates,
-                  nftName: e.target.value,
-                });
-              }}
-              required
-            />
-            <input
-              type="number"
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-900 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Supply"
-              onChange={(e) => {
-                setNFTStates({
-                  ...nftStates,
-                  supply: parseInt(e.target.value),
-                });
-              }}
-              required
-            />
-            <button
-              type="submit"
-              className="w-full py-2 px-4 bg-blue-800 hover:bg-blue-900 text-white font-medium rounded focus:outline-none focus:shadow-outline"
-            >
-              Deploy NFT
-            </button>
-          </form>
-        </div>
-      )}
+     
 
       {/* Multisender */}
-      {pageState === "3" && (
+      {pageState === "2" && (
         <div className="flex justify-center items-center mt-8 w-full px-4">
           <form
             onSubmit={handleMultisender}
